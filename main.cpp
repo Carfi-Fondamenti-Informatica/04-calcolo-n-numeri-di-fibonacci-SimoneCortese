cbc4b179261a12d36261980ad180439fe48273aa
SimoneCortese <117988407+SimoneCortese@users.noreply.github.com>
2022-11-22 23:01:05 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main() {
    int n, a=0, b=1, c=0;
    cout << "inserisci un numero intero" << endl;
    cin >> n;
-   if (n>=2); {
+   if (n>=2) {
    cout << b << endl;
       for (int i=0; i<n, i++); {
       c = a+b;
